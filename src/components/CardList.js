c5c855b6fe16250699e6d7335c7a8ee11f3ee61a
ranggaradiitya <adityarangga990@gmail.com>
2021-10-13 19:24:46 +0700

    format and changecard mapnge card manpping

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
 const CardList = ({ robots }) => {
-    return (
-        <div>
-            {
-                robots.map((user, i) => {
-                    return (
-                        <Card
-                            key={i}
-                            id={robots[i].id}
-                            name={robots[i].name}
-                            email={robots[i].email}
-                        />
-                    );
-                })
-            }
-        </div>
-    );
-}
-
-
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card key={i} id={user.id} name={user.name} email={user.email} />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CardList;
